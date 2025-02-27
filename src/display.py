b452a74b78eca95a442b5b254bc311d5bc2a39a6
TorstenTyp <torsten.stromberg@yahoo.se>
2025-02-27 22:14:51 +0100

    Self-correcting on bad data from SL

--- src/display.py
+++ src/display.py
@@ -9,6 +9,7 @@ i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
 I2C_ADDR = i2c.scan()[0]
 lcd = I2cLcd(i2c, I2C_ADDR, 2, display_width)
 speed = 0.2
+white_space = '     '
 current_task = None
 
 async def display_departures(departures):    
@@ -16,8 +17,10 @@ async def display_departures(departures):
     minutes_until_refresh = get_minutes_until_next_departure(departures)
     
     while count_down < minutes_until_refresh:
+        print(f'count_down: {count_down}')
+        print(f'minutes_until_refresh {minutes_until_refresh}')
         if 'Nu' in departures['departures'][0]['display']:
-            first_string = f"{departures['departures'][0]['line']['id']} {departures['departures'][0]['destination']} Nu"
+            first_string = f"{departures['departures'][0]['line']['id']} {departures['departures'][0]['destination']}{white_space}Nu"
         else:
             first_string = f"{departures['departures'][0]['line']['id']} {departures['departures'][0]['destination']} {minutes_until_refresh - count_down} min"
         
@@ -26,13 +29,15 @@ async def display_departures(departures):
         for departure in departures['departures']:
             if departure != departures['departures'][0]:
                 if ':' in departure['display']:
-                    second_string += f"{departure['line']['id']} {departure['destination']} {departure['display']}     "
+                    second_string += f"{departure['line']['id']} {departure['destination']} {departure['display']}{white_space}"
                 if 'Nu' in departure['display']:
-                   second_string += f"{departure['line']['id']} {departure['destination']} Nu     "
+                   second_string += f"{departure['line']['id']} {departure['destination']} Nu{white_space}"
                 if 'min' in departure['display']:
                     minutes = int(departure['display'][:-4])
-                    second_string += f"{departure['line']['id']} {departure['destination']} {minutes - count_down} min     "
-
+                    second_string += f"{departure['line']['id']} {departure['destination']} {minutes - count_down} min{white_space}"
+        
+        if '-' in second_string:
+            return
         second_string = second_string[:-4]
         show_first_string(first_string)
         lcd.move_to(0, 1)
@@ -41,13 +46,7 @@ async def display_departures(departures):
         global current_task
         if current_task and not current_task.done():
             current_task.cancel()
-            print('task cancelled')
-            try:
-                await current_task
-            except asyncio.CancelledError:
-                print('error')
-                pass
-        
+         
         current_task = asyncio.create_task(show_second_string(second_string))
              
         count_down = count_down + 1
@@ -56,6 +55,7 @@ async def display_departures(departures):
 def show_first_string(first_string):
     lcd.clear()
     lcd.move_to(0, 0)
+    first_string = first_string[:-6] + (' ' * (display_width - len(first_string[:-6]) - 6)) + first_string[-6:]
     first_string = swedify_string(first_string)
     lcd.putstr(first_string)
  
@@ -78,7 +78,7 @@ async def show_second_string(original_string):
 def get_minutes_until_next_departure(departures):
     if ':' in departures['departures'][0]['display']:
         minutes_until_refresh = 10
-    if 'u' in departures['departures'][0]['display']:
+    if 'Nu' in departures['departures'][0]['display']:
         minutes_until_refresh = 1
     if 'min' in departures['departures'][0]['display']:
         minutes_until_refresh = int(departures['departures'][0]['display'][:-4])
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from departures import get_departures
 from display import display_departures, get_minutes_until_next_departure
 import config
 import uasyncio as asyncio
+from time import sleep
 
 wifi_name = config.WIFI_NAME
 wifi_password = config.WIFI_PASSWORD
@@ -13,9 +14,12 @@ connect_to_wifi(wifi_name, wifi_password)
 
 async def main():
     while True:
-        print('Fetching departures..')
-        current_departures = await get_departures(station, direction)
-        current_departures = current_departures.json()
-        asyncio.run(display_departures(current_departures))
-    
+        try:
+            current_departures = await get_departures(station, direction)
+            current_departures = current_departures.json()
+            asyncio.run(display_departures(current_departures))
+        except Exception as e:
+            print(f'Error fetching departures: {e}')
+            sleep(5)
+
 asyncio.run(main())
